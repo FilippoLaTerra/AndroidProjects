43e27c398edf0d6f5d46f99a6a75ad108ea75d57
FilippoLaTerra <94466581+FilippoLaTerra@users.noreply.github.com>
2021-12-01 18:41:33 +0100

    cleaned repository

---
+++ E02Calcolatrice/app/src/main/java/com/example/e02_calcolatrice/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.e02_calcolatrice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+public class MainActivity : AppCompatActivity() {
+
+    TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
