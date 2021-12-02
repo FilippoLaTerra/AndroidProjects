8a0c3ecce538714e434dea2a6f9d082ca53f3837
unknown <filippo.laterra@itsvolta.it>
2021-12-02 13:30:25 +0100

    worked on dadi

--- E05Dadi/app/src/main/java/com/example/e05_dadi/MainActivity.java
+++ E05Dadi/app/src/main/java/com/example/e05_dadi/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.e05_dadi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.contentcapture.DataShareWriteAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    Drawable spiteDadi[] = new Drawable[]{  };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
