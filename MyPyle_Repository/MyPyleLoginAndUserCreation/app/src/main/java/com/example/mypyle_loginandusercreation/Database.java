36af23b10c9407d11e3ec8a4b77a64bbd4fcace2
unknown <filippo.laterra@itsvolta.it>
2021-12-07 16:54:53 +0100

    modified myPyle to use sigleton, must finish

--- MyPyle_Repository/MyPyleLoginAndUserCreation/app/src/main/java/com/example/mypyle_loginandusercreation/Database.java
+++ MyPyle_Repository/MyPyleLoginAndUserCreation/app/src/main/java/com/example/mypyle_loginandusercreation/Database.java
@@ -2,14 +2,24 @@ package com.example.mypyle_loginandusercreation;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 public class Database implements Serializable {
 
+    private static Database database = null;
+
     private List<User> userDatabase;
     protected int lastIdUsed;
 
-    public Database() {
+    public static Database getInstance(){
+        if(database == null){
+            database = new Database();
+        }
+        return database;
+    }
+
+    private Database() {
         this.userDatabase = new ArrayList<>();
         lastIdUsed = 0;
     }
