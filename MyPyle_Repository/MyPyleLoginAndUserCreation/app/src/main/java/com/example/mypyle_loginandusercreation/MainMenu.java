20ffafb9681d8d5094dad88accc80d2483808df6
FilippoLaTerra <94466581+FilippoLaTerra@users.noreply.github.com>
2021-12-14 22:18:57 +0100

    sliglty modified MyPyle Project

--- MyPyle_Repository/MyPyleLoginAndUserCreation/app/src/main/java/com/example/mypyle_loginandusercreation/MainMenu.java
+++ MyPyle_Repository/MyPyleLoginAndUserCreation/app/src/main/java/com/example/mypyle_loginandusercreation/MainMenu.java
@@ -3,14 +3,28 @@ package com.example.mypyle_loginandusercreation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
+
+    ImageView menuButtonImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         getSupportActionBar().hide();
+
+        menuButtonImageView = findViewById(R.id.menuButtonImageView);
+
+        menuButtonImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
